f793a53ff917c2493d0d47c4a00467408e9b984e
wskeee <361733529@qq.com>
2016-08-22 09:10:49 +0800

    一、项目管理 1、修改权限管理

--- common/wskeee/rbac/RbacManager.php
+++ common/wskeee/rbac/RbacManager.php
@@ -8,13 +8,12 @@
 
 namespace wskeee\rbac;
 
-use yii\rbac\DbManager;
-use yii\rbac\Item;
-use yii\db\Query;
-use yii\caching\Cache;
-use yii\helpers\ArrayHelper;
-
 use common\models\User;
+use Yii;
+use yii\caching\Cache;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
+use yii\rbac\DbManager;
 /**
  * Description of RbacManager
  *
@@ -27,11 +26,6 @@ class RbacManager extends DbManager{
         parent::init();
         $this->loadFromCache();
     }
-    /*
-     * 角色所包括的用户
-     * @var type role => [user,user]
-     */
-    protected $itemToUsers;
 
     /**
      *
@@ -39,12 +33,8 @@ class RbacManager extends DbManager{
      */
     protected $childs;
     
-    /**
-     * 
-     * @var array [item_name => user]
-     */
-    protected $assignmentToUsers;
-
+    protected $assignmentsCache = [];
+    
     public function loadFromCache()
     {
         if ($this->items !== null || !$this->cache instanceof Cache) {
@@ -53,8 +43,8 @@ class RbacManager extends DbManager{
 
         $data = $this->cache->get($this->cacheKey);
         
-        if (is_array($data) && isset($data[0], $data[1], $data[2], $data[3])) {
-            list ($this->items, $this->rules, $this->parents,$this->itemToUsers) = $data;
+        if (is_array($data) && isset($data[0], $data[1], $data[2])) {
+            list ($this->items, $this->rules, $this->parents) = $data;
             return;
         }
         
@@ -77,51 +67,57 @@ class RbacManager extends DbManager{
             if (isset($this->items[$row['child']])) {
                 $this->parents[$row['child']][] = $row['parent'];
             }
-            /*
+            
             if (isset($this->items[$row['parent']])) {
                 $this->childs[$row['parent']][] = $row['child'];
-            }*/
+            }
         }
         
         //create roleToUsers;
-        $this->itemToUsers = [];
-        $this->assignmentToUsers = [];
-         
+        //$this->$assignments = [];
+        /* 
         $query = (new Query)->from($this->assignmentTable);
         foreach ($query->all($this->db) as $row)
         {
             if(isset($this->items[$row['item_name']]))
-                $this->assignmentToUsers[$row['item_name']][] = $row['user_id'];
-        }
-        $result;
-        foreach ($this->items as $itemName => $item)
-        {
-            $result = [];
-            $this->getItemUser($itemName, $result);
-            $this->itemToUsers[$itemName] = array_unique($result);
-        }
+                $this->$assignments[$row['user_id']][] = $row['item_name'];
+        }*/
         
-        $this->cache->set($this->cacheKey, [$this->items, $this->rules, $this->parents,$this->itemToUsers]);
+        $this->cache->set($this->cacheKey, [$this->items, $this->rules, $this->parents]);
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function getAssignments($userId)
+    {
+        if(isset($this->assignmentsCache[$userId]))
+            return $this->assignmentsCache[$userId];
+        else{
+            $assignments = parent::getAssignments($userId);
+            $this->assignmentsCache[$userId] = $assignments;
+            return $assignments;
+        }
     }
     
     /**
      * 获取属于该角色或者权限的所有用户
      * @param string $itemName  目标角色或者权限
-     * @param array $result     最终结果,所甩用户集合
+     * @param array $result     []
+     * @return 最终结果,所甩用户集合
      */
-    protected function getItemUser($itemName,&$result)
+    protected function getItemUser($itemName,$result)
     {
-        if(isset($this->assignmentToUsers[$itemName]))
+        $atousers = isset($this->assignmentToUsers[$itemName]) ? $this->assignmentToUsers[$itemName] : [];
+        $result = array_unique(ArrayHelper::merge($atousers,$result));
+        if(isset($this->childs[$itemName]))
         {
-            $result = ArrayHelper::merge($this->assignmentToUsers[$itemName],$result);
-            if(isset($this->parents[$itemName]))
-            {
-                foreach ($this->parents[$itemName] as $child)
-                    $this->getItemUser($child,$result);
-            }
+            foreach ($this->childs[$itemName] as $child)
+                $result = array_unique(ArrayHelper::merge($this->getItemUser($child,$result),$result));
         }
+        return $result;
     }
-    
+
     /**
      * 获取拥有该角色或者权限所有用户，<br/>
      * 比如，获取所有【编导】，或者所有能【创建预约】的用户
@@ -130,10 +126,26 @@ class RbacManager extends DbManager{
      */
     public function getItemUsers($itemName)
     {
-        $result = [];
-        if(isset($this->itemToUsers[$itemName]))
-            $result = User::findAll(['id'=>  $this->itemToUsers[$itemName]]);
+        $result = User::find()
+                ->leftJoin(['Assignment'=> $this->assignmentTable],"Assignment.user_id = id")
+                ->where(['Assignment.item_name'=>$itemName])
+                ->all();
         return $result;
+    }
+    /**
+     * 获取拥有该角色或者权限所有用户，<br/>
+     * 比如，获取所有【编导】，或者所有能【创建预约】的用户
+     * @param string $itemName  角色或者权限 [User]
+     * @return array [id=>name,id=>name]
+     */
+    public function getItemUserList($itemName){
+        $result = (new Query())
+                ->select(['User.id','User.nickname'])
+                ->from(['User'=>  User::tableName()])
+                ->leftJoin(['Assignment'=> $this->assignmentTable],"Assignment.user_id = User.id")
+                ->where(['Assignment.item_name'=>$itemName])
+                ->all(Yii::$app->db);
+        return ArrayHelper::map($result, 'id', 'nickname');
     }
     
     /**
@@ -144,15 +156,6 @@ class RbacManager extends DbManager{
      */
     public function isRole($roleName,$userId)
     {
-        /* @var $user User */
-        if(isset($this->itemToUsers[$roleName]))
-        {
-            foreach($this->itemToUsers[$roleName] as $uid)
-            {
-                if($uid == $userId)
-                    return true;
-            }
-        }
-        return false;
+        return $this->checkAccess($userId, $roleName);
     }
 }
