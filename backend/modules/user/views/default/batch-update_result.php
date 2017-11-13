a71476f7d4c646e9396cea5fbcfedfbe43be79f7
wskeee <361733529@qq.com>
2017-11-13 09:50:41 +0800

    1、删除多余模块 2、增加指量导入用户模块 3、删除查看前端页功能

--- backend/modules/user/views/default/batch-update_result.php
+++ backend/modules/user/views/default/batch-update_result.php
@@ -14,9 +14,14 @@ use yii\web\View;
 $this->title = '用户数据导入结果';
 $right = '<span style="color:#0000ff">√</span>';
 $wrong = '<span style="color:#ff0000">×（用户名重复）</span>';
+$rightNum = 0;
+foreach($users as $user){
+    !$user[3] ? : $rightNum++;
+}
 ?>
 <div class="user-import-index">
     <h3><?= $this->title  ?></h3>
+    <h4>共有<?= count($users) ?> 个用户需要导入，成功导入 <?= $rightNum ?> 个，失败<span style="color: #FF0000"><?= count($users) - $rightNum ?></span>个</h4>
     <?php if (count($users) > 0): ?>
         <table border="1" class="table table-striped table-bordered">
             <tr>
